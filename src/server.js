1b1b32014cfec9e63be29870236ee3ca8c0e194a
Ivan Sergiienko <darthvanger@gmail.com>
2022-05-07 23:33:24 +0300

    Add express server

---
+++ src/server.js
@@ -0,0 +1,20 @@
+const https = require('https')
+const fs = require('fs')
+const express = require('express')
+const app = express()
+const port = 443
+
+app.use(express.static('build'))
+
+var privateKey = fs.readFileSync( 'key.pem' );
+var certificate = fs.readFileSync( 'cert.pem' );
+
+https.createServer({
+            key: privateKey,
+            cert: certificate
+}, app).listen(port);
+
+app.get('/', (req, res) => {
+          res.send('Hello World!')
+})
+~                  
