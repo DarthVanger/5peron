341cdfecd18bf11c79562f274f640ea2a43fc3d6
Ivan Sergiienko <darthvanger@gmail.com>
2022-05-07 20:53:40 +0300

    Create hamburger menu, insert img, video

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import logo from './logo.svg';
+//import srcVideo from '../public/video/5peron-truck-unloading.mp4';
 import './App.css';
+import MenuSvg from './MenuSvg';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <nav>
+          <div>
+            <a>L</a>
+            <div className="nav-right">
+              <button aria-label="Toggle menu button" className="menu-button">
+                <MenuSvg />
+              </button>
+            </div>
+
+          </div>
+        </nav>
       </header>
+      <img
+        src={process.env.PUBLIC_URL + '/img/peron-image.jpg'}
+        alt="5peron picture"
+        className="peron-image" />
+      <h1>Peron Five </h1>
+      <video width="320" height="240" controls>
+         <source src={process.env.PUBLIC_URL + '/video/5peron-truck-unloading.mp4'} />
+      </video>
     </div>
   );
 }
---
+++ src/MenuSvg.js
@@ -0,0 +1,7 @@
+const MenuSvg = () => (
+  <svg className="menu-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 40">
+    <path d="M 2 13 l 10 0 l 0 0 l 10 0 M 4 19 l 8 0 M 12 19 l 8 0 M 2 25 l 10 0 l 0 0 l 10 0" />
+  </svg>
+);
+
+export default MenuSvg;
