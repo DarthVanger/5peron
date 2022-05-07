759c51a0abd9a93f84657ab49e1b02b7a65a409d
Ivan Sergiienko <darthvanger@gmail.com>
2022-05-07 22:14:17 +0300

    Make a cool first page :)

--- src/App.js
+++ src/App.js
@@ -1,29 +1,32 @@
-import logo from './logo.svg';
+import logo from './train-station-icon.jpg';
 //import srcVideo from '../public/video/5peron-truck-unloading.mp4';
 import './App.css';
 import MenuSvg from './MenuSvg';
+import trainColorful from './train-colorful.jpg';
 
 function App() {
   return (
     <div className="App">
       <header>
         <nav>
-          <div>
-            <a>L</a>
-            <div className="nav-right">
-              <button aria-label="Toggle menu button" className="menu-button">
-                <MenuSvg />
-              </button>
-            </div>
-
+          <div className="logo-container">
+            <img src={logo} alt="logo" className="logo" />
+          </div>
+          <div className="brand">
+            Platform Five
+          </div>
+          <div className="nav-right">
+            <button aria-label="Toggle menu button" className="menu-button">
+              <MenuSvg />
+            </button>
           </div>
         </nav>
       </header>
       <img
-        src={process.env.PUBLIC_URL + '/img/peron-image.jpg'}
+        src={trainColorful}
         alt="5peron picture"
         className="peron-image" />
-      <h1>Peron Five </h1>
+      <h1>Platform 5</h1>
       <video width="320" height="240" controls>
          <source src={process.env.PUBLIC_URL + '/video/5peron-truck-unloading.mp4'} />
       </video>
