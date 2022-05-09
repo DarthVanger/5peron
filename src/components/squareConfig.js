41fc021b9a49afd8eb24de43a972d36984a39206
Ivan Sergiienko <darthvanger@gmail.com>
2022-05-10 02:49:02 +0300

    Add square apple verification files

---
+++ src/components/squareConfig.js
@@ -0,0 +1,5 @@
+export const squareConfig = {
+  appId: 'sandbox-sq0idb-LP1vADLnDtiJeOBuv_FfJw',
+  secretToken: 'EAAAEIaX4SQfLcsDpXPEdDePGYQ95StkABmo4veu3CR47mC1No1pCxtX32Jw_HWg',
+  locationId: 'LHHDJ69G41XTR',
+};
