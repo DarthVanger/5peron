c33c4bbb5e5a4c5c31ffabd5d4554856a2fdc21f
Ivan Sergiienko <darthvanger@gmail.com>
2022-05-12 02:29:07 +0300

    Remove PayPal button

--- src/components/Donate.js
+++ src/components/Donate.js
@@ -13,47 +13,10 @@ export const Donate = () => {
   const rendered = useRef();
   const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
-    const script = document.createElement("script");
-    script.src = "https://www.paypalobjects.com/donate/sdk/donate-sdk.js";
-    script.async = true;
-    document.body.appendChild(script);
-
-    setTimeout(() => { 
-      if (rendered.current) return;
-      setIsLoading(false);
-      rendered.current = true;
-
-      window.PayPal.Donation.Button({
-        env:'production',
-        hosted_button_id:'JLCJ8UU8Z6L5Y',
-        image: {
-          src: donateImg,
-          alt:'Donate with PayPal button',
-          title:'PayPal - The safer, easier way to pay online!',
-          }
-        }).render('#donate-button');
-
-    }, 2000);
-  }, []);
-
   return (
     <>
       <div className="donate">
-        <div className="image-placeholder">
-          <img src={donateImg} alt="hearts and hands drawing" />
-        </div>
-        <div className="paypal center-absolute">
-          <div id="donate-button-container">
-            <div id="donate-button">
-            </div>
-          </div>
-        </div>
-        <div className="button-container center-absolute">
-          <button type="button">
-            Donate with PayPal
-          </button>
-        </div>
+        <img src={donateImg} alt="hearts and hands drawing" />
       </div>
       <div className="google-pay-container">
         <GooglePay className="google-pay" />
