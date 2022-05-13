1b64d048921a3546fd44df48099579c58d693a54
Ivan Sergiienko <darthvanger@gmail.com>
2022-05-13 21:53:53 +0300

    Add description for refugees article

--- src/components/articles/HostingRefugees.js
+++ src/components/articles/HostingRefugees.js
@@ -3,6 +3,7 @@ import imgSrc from './orphan-kid.jpeg';
 export const HostingRefugees = () => (
   <article>
     <h2>Hosted 550 adult and child refugees</h2>
+    <p>Platform Five has provided temporary housing for 550 refugees, incuding 230 children</p>
     <img src={imgSrc} />
   </article>
 );
