1c4b4a63af579df18962fc3a81d0e35a5c596ad6
Ivan Sergiienko <darthvanger@gmail.com>
2022-05-17 02:46:01 +0300

    Create component for dear volunteers article
    The article itself is not shown right now though

---
+++ src/components/articles/DearVolunteer.js
@@ -0,0 +1,23 @@
+export const DearVolunteers = () => (
+  <article id="dear-volunteers">
+    <h2>Volunteers of the 5th platform!</h2>
+
+    <p>
+      Our Life, just as the life of any Ukrainian, has changed on February the 24th due to the full-scale invasion of Russia into Ukraine.
+    </p>
+
+    <figure>
+      <img src={building} alt="destroyed building in ukraine" />
+      <figcaption>
+        pic source: <a
+          href="https://www.dailysabah.com/arts/artists-use-nfts-to-support-ukraine-against-russian-invasion/news"
+          target="_blank"
+          rel="noref"
+        >
+          dalisabah.com/arts
+        </a>
+      </figcaption>
+    </figure>
+
+  </article>
+);
