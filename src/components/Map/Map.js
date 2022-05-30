5163f82163a4a3c19f41b4dffb2d6b28617c4492
Ivan Sergiienko <darthvanger@gmail.com>
2022-05-31 01:14:18 +0300

    Add click listener :)

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,15 +1,48 @@
 import { useEffect, useState, useRef } from 'react';
 import icon from '../insta-icon.png';
+import useDeepCompareEffect from 'use-deep-compare-effect'
 
 export const Map = () => {
   const ref = useRef(null);
   const [map, setMap] = useState();
+  const [mapOptions, setMapOptions] = useState({});
+
+  const onClick = (e) => {
+    console.log('click', e);
+    const { latLng } = e;
+  };
+
+  const onIdle = (e) => {
+    console.log('idle', e);
+  };
+
+    // because React does not do deep comparisons, a custom hook is used
+  // see discussion in https://github.com/googlemaps/js-samples/issues/946
+  useDeepCompareEffect(() => {
+    if (map) {
+      map.setOptions(mapOptions);
+    }
+  }, [map, mapOptions]);
+
+  useEffect(() => {
+    if (map) {
+      ["click", "idle"].forEach((eventName) =>
+        window.google.maps.event.clearListeners(map, eventName)
+      );
+
+      if (onClick) {
+        map.addListener("click", onClick);
+      }
+
+      if (onIdle) {
+        map.addListener("idle", () => onIdle(map));
+      }
+    }
+}, [map, onClick, onIdle]);
 
   console.log('render map');
   useEffect(() => {
     if (ref.current && !map) {
-      console.log('set map');
-      console.log('ref.current: ', ref.current);
       // https://stackoverflow.com/questions/11749742/google-maps-api-v3-gray-box-no-map
       var latlng = new window.google.maps.LatLng(-34.397, 150.644);
       var mapOptions = 
@@ -19,22 +52,12 @@ export const Map = () => {
           //backgroundColor: '#ff0000',
           mapTypeId: window.google.maps.MapTypeId.ROADMAP,
           //imageDefaultUI: true
+          onClick: onClick,
       };
       setMap(new window.google.maps.Map(ref.current, mapOptions));
+      setMapOptions(mapOptions);
     }
   }, [ref, map]);
-
-  //useEffect(() => {
-  //  setTimeout(() => {
-  //    if (ref.current && !map) {
-  //      console.log('set map');
-  //      setMap(new window.google.maps.Map(ref.current, {}));
-  //      setTimeout(() => {
-  //        window.google.maps.event.trigger(map, 'resize');
-  //      }, 1000);
-  //    }
-  //  }, 2000);
-  //}, [map]);
 
   return (
     <div ref={ref} style={{height: '100vh', width: '100wv' }} />
