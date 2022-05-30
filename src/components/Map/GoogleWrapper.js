6b41c92352a3efb4b96c0c49441fbb20f76423a5
Ivan Sergiienko <darthvanger@gmail.com>
2022-05-31 01:24:57 +0300

    Display markers on click! :)

--- src/components/Map/GoogleWrapper.js
+++ src/components/Map/GoogleWrapper.js
@@ -1,5 +1,7 @@
+import React, { useEffect, useState, useRef } from 'react';
 import { Wrapper, Status } from "@googlemaps/react-wrapper";
 import { Map } from './Map';
+import { Marker } from './Marker';
 
 const apiKey = 'AIzaSyA2UDFg1D25cvlIZICfzo9peR4cfoxo_Lg';
 
@@ -7,8 +9,35 @@ const render = (status) => {
   return <h1>{status}</h1>;
 };
 
-export const GoogleWrapper  = ({children}) => (
-  <Wrapper apiKey={apiKey} render={render}>
-    <Map />
-  </Wrapper>
-);
+export const GoogleWrapper  = () => {
+  const [clicks, setClicks] = React.useState([]);
+  const [zoom, setZoom] = React.useState(3); // initial zoom
+  const [center, setCenter] = React.useState({
+    lat: 0,
+    lng: 0,
+  });
+
+  const onClick = (e) => {
+    // avoid directly mutating state
+    setClicks([...clicks, e.latLng]);
+  };
+
+  const onIdle = (m) => {
+    console.log("onIdle");
+    setZoom(m.getZoom());
+    setCenter(m.getCenter().toJSON());
+  };
+
+  return (
+    <Wrapper apiKey={apiKey} render={render}>
+      <Map
+        center={center}
+        onClick={onClick}
+        onIdle={onIdle}
+        zoom={zoom}
+      >
+        {clicks.map((latLng, i) => (<Marker key={i} position={latLng} />))}
+      </Map>
+    </Wrapper>
+  );
+};
--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,28 +1,24 @@
-import { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import icon from '../insta-icon.png';
 import useDeepCompareEffect from 'use-deep-compare-effect'
 
-export const Map = () => {
+export const Map = ({
+  onClick,
+  onIdle,
+  children,
+  style,
+  ...options
+}) => {
   const ref = useRef(null);
   const [map, setMap] = useState();
-  const [mapOptions, setMapOptions] = useState({});
 
-  const onClick = (e) => {
-    console.log('click', e);
-    const { latLng } = e;
-  };
-
-  const onIdle = (e) => {
-    console.log('idle', e);
-  };
-
-    // because React does not do deep comparisons, a custom hook is used
+  // because React does not do deep comparisons, a custom hook is used
   // see discussion in https://github.com/googlemaps/js-samples/issues/946
   useDeepCompareEffect(() => {
     if (map) {
-      map.setOptions(mapOptions);
+      map.setOptions(options);
     }
-  }, [map, mapOptions]);
+  }, [map, options]);
 
   useEffect(() => {
     if (map) {
@@ -40,26 +36,22 @@ export const Map = () => {
     }
 }, [map, onClick, onIdle]);
 
-  console.log('render map');
   useEffect(() => {
     if (ref.current && !map) {
-      // https://stackoverflow.com/questions/11749742/google-maps-api-v3-gray-box-no-map
-      var latlng = new window.google.maps.LatLng(-34.397, 150.644);
-      var mapOptions = 
-      {
-          zoom: 8,
-          center:latlng,
-          //backgroundColor: '#ff0000',
-          mapTypeId: window.google.maps.MapTypeId.ROADMAP,
-          //imageDefaultUI: true
-          onClick: onClick,
-      };
-      setMap(new window.google.maps.Map(ref.current, mapOptions));
-      setMapOptions(mapOptions);
+      setMap(new window.google.maps.Map(ref.current, {}));
     }
   }, [ref, map]);
 
+  // https://developers.google.com/maps/documentation/javascript/react-map#javascript_1
   return (
-    <div ref={ref} style={{height: '100vh', width: '100wv' }} />
+    <>
+      <div ref={ref} style={{height: '100vh', width: '100wv' }} />
+      {React.Children.map(children, (child) => {
+        if (React.isValidElement(child)) {
+          // set the map prop on the child component
+          return React.cloneElement(child, { map });
+        }
+      })}
+    </>
   );
 };
